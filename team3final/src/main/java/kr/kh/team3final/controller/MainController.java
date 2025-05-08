06d5125b519a8b7b63a8533e483c6219b77b9857
heo-daejoong <dygks5787@naver.com>
2025-05-08 18:00:39 +0900

    파이널 숙소 정보 페이지 중

--- team3final/src/main/java/kr/kh/team3final/controller/MainController.java
+++ team3final/src/main/java/kr/kh/team3final/controller/MainController.java
@@ -2,6 +2,8 @@ package kr.kh.team3final.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -10,6 +12,11 @@ public class MainController {
 	@GetMapping("/")
 	public String main() {
 		return "index";
+	}
+	
+	@GetMapping("/reserv")
+	public String reserv() {
+		return "reserv";
 	}
 	
 }
