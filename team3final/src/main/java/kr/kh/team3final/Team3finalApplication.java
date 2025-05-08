4ded8696ca86558b50b01eaebc37599183c1d6be
Soon17 <acejsh77@naver.com>
2025-05-08 14:10:26 +0900

    기본 셋팅(DB연동X, 시큐리티X)

---
+++ team3final/src/main/java/kr/kh/team3final/Team3finalApplication.java
@@ -0,0 +1,13 @@
+package kr.kh.team3final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3finalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team3finalApplication.class, args);
+	}
+
+}
---
+++ team3final/src/test/java/kr/kh/team3final/Team3finalApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kh.team3final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3finalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
