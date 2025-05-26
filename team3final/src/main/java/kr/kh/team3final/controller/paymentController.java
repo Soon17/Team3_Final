e4cbed021bb8c0c9dbde38fc9285d0a5503fc46c
yupseunghan <sky690212@gmail.com>
2025-05-26 17:51:42 +0900

    예약 및 결제 기능 구현중

--- team3final/src/main/java/kr/kh/team3final/controller/paymentController.java
+++ team3final/src/main/java/kr/kh/team3final/controller/paymentController.java
@@ -1,23 +1,37 @@
 package kr.kh.team3final.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.kh.team3final.model.vo.LodgingVO;
 import kr.kh.team3final.service.LodgingService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("/pay")
 public class PaymentController {
+	
+	@Value("${kakao.pay-store}")
+    private String store;
+
+	@Value("${kakao.pay-channel}")
+    private String channel;
 
 	@Autowired
 	LodgingService lodgingService;
 
-	@GetMapping("/payment")
+	@GetMapping("/payment") 
 	public String payment(Model model,@RequestParam int ld_num,@RequestParam String checkTime,@RequestParam String rm_person) {
 		LodgingVO lodging=  lodgingService.getLodging(ld_num);
 		String[] parts = checkTime.split(" ~ ");
@@ -31,7 +45,8 @@ public class PaymentController {
 	}
 
 	@GetMapping("/summary")
-	public String paymentSummary() {
+	public String paymentSummary(Model model,@RequestParam String rm_name) {
+		model.addAttribute("rm_name", rm_name);
 		return "pay/summary";
 	}
     private static String parseDate(String checkTime) {
@@ -44,4 +59,13 @@ public class PaymentController {
 
         return month + "/" + day + "/" + dayOfWeek;
     }
+	@PostMapping("/kakao/apiKey")
+	@ResponseBody
+	public Map<String, Object> getApi(){
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("channel", channel);
+		map.put("store", store);
+		return map;
+	}
+	
 }
