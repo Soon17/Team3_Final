9f31f1ef03990655540fc0ba256f04a93f2fdce7
yupseunghan <sky690212@gmail.com>
2025-05-28 16:11:29 +0900

    팀원 merge

--- team3final/src/main/java/kr/kh/team3final/dao/ReviewDAO.java
+++ team3final/src/main/java/kr/kh/team3final/dao/ReviewDAO.java
@@ -10,8 +10,6 @@ import kr.kh.team3final.model.vo.LodgingVO;
 
 public interface ReviewDAO {
 
-<<<<<<< HEAD
-	List<Lodging_ReviewDTO> getSelectReviewList();
 
 	List<Lodging_ReviewDTO> selectReview(@Param("rv_table_name") String rv_table_name, @Param("ld_num") int ld_num);
 
@@ -19,8 +17,8 @@ public interface ReviewDAO {
 
 	List<Map<String, Object>> selectRatingCounts(@Param("ld_num") int ld_num);
 
-=======
+
 	List<Lodging_ReviewDTO> getSelectReviewList(int meNum);
 	
->>>>>>> origin/hdj
+
 }
