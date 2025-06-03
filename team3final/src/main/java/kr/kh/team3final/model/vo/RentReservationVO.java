6b00dce35c4b46e3ccb7b28b14debc4066a4c0e1
Soon17 <acejsh77@naver.com>
2025-06-03 17:10:24 +0900

    허대중님 멀쥐

--- team3final/src/main/java/kr/kh/team3final/model/vo/RentReservationVO.java
+++ team3final/src/main/java/kr/kh/team3final/model/vo/RentReservationVO.java
@@ -14,7 +14,7 @@ private int rr_price;  //렌트가격
 private Date rr_date; //대여일자
 private String rr_state; //예약상태(예약취소, 완료, 환불)
 private String rr_name; //예약자이름
-private int rr_number; //예약자 전화번호
+private String rr_number; //예약자 전화번호
 private int rr_birth; //예약자 생년월일
 private int rr_cp_num; //예약 차량 패키지 번호
 private int rr_me_num; //예약자 유저넘버
